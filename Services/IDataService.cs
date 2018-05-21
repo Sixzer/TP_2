108b3554e5903e6dae3c9c859a5392d8d7407507
Sixzer <39485657+Sixzer@users.noreply.github.com>
2018-05-21 14:31:17 +0300

    Initial commit

---
+++ Services/IDataService.cs
@@ -0,0 +1,17 @@
+﻿using LabaApp.Model;
+
+namespace LabaApp.Services
+{
+    /// <summary>
+    /// Интерфейс получения данных приложения.
+    /// </summary>
+    public interface IDataService
+    {
+        /// <summary>
+        /// Получить корневой элемент динамической структуры.
+        /// </summary>
+        /// <param name="fileName">Файл с данными структуры.</param>
+        /// <returns>Корневой элемент структуры.</returns>
+        Node GetData(string fileName);
+    }
+}
---
+++ Services/XmlSerializationService.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using LabaApp.Model;
+using LabaApp.Model.Dto;
+using System.Xml.Serialization;
+
+namespace LabaApp.Services
+{
+    /// <inheritdoc/>
+    class XmlSerializationService : ISerializationService<Node>
+    {
+        /// <inheritdoc/>
+        private XmlSerializer _xmlSerializer;
+
+        public XmlSerializationService()
+        {
+            _xmlSerializer = new XmlSerializer(typeof(NodeDto));
+        }
+
+        /// <inheritdoc/>
+        public Node Deserialize(Stream stream)
+        {
+            var nodeDto = new NodeDto();
+            nodeDto = (NodeDto)_xmlSerializer.Deserialize(stream);
+            return nodeDto.Deserialize();
+        }
+
+        /// <inheritdoc/>
+        public void Serialize(Node node, Stream stream)
+        {
+            var nodeDto = new NodeDto();
+            nodeDto.Serialize(node);
+            _xmlSerializer.Serialize(stream, nodeDto);
+        }
+    }
+}
